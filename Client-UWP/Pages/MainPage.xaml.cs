8a3b32d4b9ba74885c893147106aeb5976a00d06
ivanab <ivana@opticaltone.com>
2018-11-27 18:12:14 +0100

    Added peers list connect/disconnect from server using HttpSignaler

---
+++ Client-UWP/Pages/MainPage.xaml.cs
@@ -0,0 +1,167 @@
+﻿using ClientCore.Signaling;
+using ClientCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ViewManagement;
+using System.Diagnostics;
+using Windows.UI.Popups;
+using Windows.UI.Core;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Client_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        HttpSignaler _httpSignaler = new HttpSignaler();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(450, 550);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            string name = RTCController.Instance.LocalPeer.Name;
+            Debug.WriteLine($"Connecting to server from local peer: {name}");
+
+            _httpSignaler.SignedIn += Signaler_SignedIn;
+            _httpSignaler.ServerConnectionFailed += Signaler_ServerConnectionFailed;
+            _httpSignaler.PeerConnected += Signaler_PeerConnected;
+            _httpSignaler.PeerDisconnected += Signaler_PeerDisconnected;
+
+            InitView();
+        }
+
+        private async void Signaler_SignedIn(object sender, EventArgs e)
+        {
+            // The signaler will notify all events from the signaler
+            // task thread. To prevent concurrency issues, ensure all
+            // notifications from this thread are asynchronously
+            // forwarded back to the GUI thread for further processing.
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => HandleSignedIn(sender, e));
+        }
+
+        private void HandleSignedIn(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Peer signed in to server.");
+        }
+
+        private async void Signaler_ServerConnectionFailed(object sender, EventArgs e)
+        {
+            // See method Signaler_SignedIn for concurrency comments.
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => HandleServerConnectionFailed(sender, e));
+
+        }
+
+        private void HandleServerConnectionFailed(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Server connection failure.");
+        }
+
+        private async void Signaler_PeerConnected(object sender, Peer peer)
+        {
+            // See method Signaler_SignedIn for concurrency comments.
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => HandlePeerConnected(sender, peer));
+        }
+
+        private void HandlePeerConnected(object sender, Peer peer)
+        {
+            Debug.WriteLine($"Peer connected {peer.Name} / {peer.Id}");
+
+            if (peersListView.Items.Contains(peer))
+            {
+                Debug.WriteLine($"Peer already found in list: {peer.ToString()}");
+                return;
+            }
+
+            if (RTCController.Instance.LocalPeer.Name == peer.Name)
+            {
+                Debug.WriteLine($"Peer is our local peer: {peer.ToString()}");
+                return;
+            }
+            peersListView.Items.Add(peer);
+        }
+
+        private async void Signaler_PeerDisconnected(object sender, Peer peer)
+        {
+            // See method Signaler_SignedIn for concurrency comments.
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => HandlePeerDisconnected(sender, peer));
+
+        }
+
+        private void HandlePeerDisconnected(object sender, Peer peer)
+        {
+            Debug.WriteLine($"Peer disconnected {peer.Name} / {peer.Id}");
+
+            for (int i = 0; i < peersListView.Items.Count(); i++)
+            {
+                Peer p = (Peer)peersListView.Items[i];
+                if (p.Name == peer.Name)
+                    peersListView.Items.Remove(peersListView.Items[i]);
+            }
+        }
+
+        private void InitView()
+        {
+            peersListView.SelectedIndex = -1;
+            peersListView.SelectedItem = null;
+
+            peersListView.Tapped += PeersListView_Tapped;
+
+            ConnectPeer.Click += async (sender, args) =>
+            {
+                Debug.WriteLine("Connects to server.");
+
+                await _httpSignaler.Connect();
+
+                ConnectPeer.IsEnabled = false;
+                DisconnectPeer.IsEnabled = true;
+            };
+
+            DisconnectPeer.Click += async (sender, args) =>
+            {
+                Debug.WriteLine("Disconnects from server.");
+
+                await _httpSignaler.SignOut();
+
+                peersListView.Items.Clear();
+
+                DisconnectPeer.IsEnabled = false;
+                ConnectPeer.IsEnabled = true;
+            };
+
+            CallRemotePeer.Click += async (sender, args) =>
+            {
+                if (peersListView.SelectedIndex == -1)
+                {
+                    await new MessageDialog("Please select a peer.").ShowAsync();
+                    return;
+                }
+
+                Peer remotePeer = peersListView.SelectedItem as Peer;
+                if (remotePeer == null) return;
+
+                Debug.WriteLine($"Call remote peer {remotePeer.ToString()}");
+            };
+        }
+
+        private void PeersListView_Tapped(object sender, TappedRoutedEventArgs e) =>
+            peersListView.SelectedItem = (Peer)((FrameworkElement)e.OriginalSource).DataContext;
+    }
+}
