8f0b71b41d4ca2b03bb444963234abd183e3f1c1
ivanab <ivana@opticaltone.com>
2019-03-05 18:45:11 +0100

    Added settings account editor page

---
+++ Client-UWP/Pages/SettingsAccount/SettingsAccountEditorPage.xaml.cs
@@ -0,0 +1,123 @@
+﻿using Client_UWP.Controllers;
+using Client_UWP.Models;
+using Client_UWP.Pages.SettingsConnection;
+using Client_UWP.Pages.SettingsDebug;
+using Client_UWP.Pages.SettingsDevices;
+using Client_UWP.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Client_UWP.Pages.SettingsAccount
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SettingsAccountEditorPage : Page
+    {
+        public SettingsAccountEditorPage()
+        {
+            InitializeComponent();
+
+            ViewModel = new SettingsAccountPageViewModel();
+
+            GoToSettingsAccountPage.Click += (sender, args) => Frame.Navigate(typeof(SettingsConnectionPage));
+
+            ConnectionSettings.Click += (sender, args) => Frame.Navigate(typeof(SettingsConnectionPage));
+
+            DevicesSettings.Click += (sender, args) => Frame.Navigate(typeof(SettingsDevicesPage));
+
+            DebugSettings.Click += (sender, args) => Frame.Navigate(typeof(SettingsDebugPage));
+        }
+
+        public SettingsAccountPageViewModel ViewModel { get; set; }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Account account = (Account)e.Parameter;
+
+            if (e.Parameter != null)
+            {
+                // Add new Account to AccountsList
+                ViewModel.AccountsList.Add(account);
+
+                // Save AccountsList
+                SettingsController.Instance.localSettings.Values["AccountsList"] =
+                    XmlSerialization<ObservableCollection<Account>>.Serialize(ViewModel.AccountsList);
+
+                Debug.WriteLine("Edit Account: " + account.AccountName);
+
+                tbAccountName.Text = account.AccountName;
+                tbIP.Text = account.IP;
+                tbPort.Text = account.Port;
+                btnAdd.Visibility = Visibility.Collapsed;
+                btnSave.Visibility = Visibility.Visible;
+                btnDelete.Visibility = Visibility.Visible;
+
+                btnDelete.Click += (sender, args) =>
+                {
+                    // Remove Account from AccountsList
+                    ViewModel.AccountsList.Remove(account);
+
+                    // Save AccountsList
+                    SettingsController.Instance.localSettings.Values["AccountsList"] =
+                        XmlSerialization<ObservableCollection<Account>>.Serialize(ViewModel.AccountsList);
+
+                    Frame.Navigate(typeof(SettingsAccountPage));
+                };
+
+                btnSave.Click += (sender, args) =>
+                {
+                    // Remove Account from AccountsList
+                    ViewModel.AccountsList.Remove(account);
+
+                    // Add Account to AccountsList
+                    ViewModel.AccountsList.Add(new Account
+                    {
+                        AccountName = tbAccountName.Text,
+                        IP = tbIP.Text,
+                        Port = tbPort.Text
+                    });
+
+                    // Save AccountsList
+                    SettingsController.Instance.localSettings.Values["AccountsList"] =
+                        XmlSerialization<ObservableCollection<Account>>.Serialize(ViewModel.AccountsList);
+
+                    Frame.Navigate(typeof(SettingsAccountPage));
+                };
+            }
+
+            btnAdd.Click += (sender, args) =>
+            {
+                // Add new Account to AccountsList
+                ViewModel.AccountsList.Add(new Account
+                {
+                    AccountName = tbAccountName.Text,
+                    IP = tbIP.Text,
+                    Port = tbPort.Text
+                });
+
+                // Save AccountsList
+                SettingsController.Instance.localSettings.Values["AccountsList"] =
+                    XmlSerialization<ObservableCollection<Account>>.Serialize(ViewModel.AccountsList);
+
+                Frame.Navigate(typeof(SettingsAccountPage));
+            };
+        }
+    }
+}
--- Client-UWP/Pages/SettingsAccount/SettingsAccountPage.xaml.cs
+++ Client-UWP/Pages/SettingsAccount/SettingsAccountPage.xaml.cs
@@ -1,14 +1,19 @@
 ﻿using Client_UWP.Controllers;
+using Client_UWP.Models;
 using Client_UWP.Pages.SettingsConnection;
 using Client_UWP.Pages.SettingsDebug;
 using Client_UWP.Pages.SettingsDevices;
+using Client_UWP.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -41,13 +46,35 @@ namespace Client_UWP.Pages.SettingsAccount
         {
             GoToMainPage.Click += (sender, args) => Frame.Navigate(typeof(MainPage));
 
+            ConnectionSettings.Click += (sender, args) => Frame.Navigate(typeof(MainPage));
+
             DevicesSettings.Click += (sender, args) => Frame.Navigate(typeof(SettingsDevicesPage));
 
             DebugSettings.Click += (sender, args) => Frame.Navigate(typeof(SettingsDebugPage));
 
+            AddAccount.Click += (sender, args) => Frame.Navigate(typeof(SettingsAccountEditorPage));
 
-            AddAccount.Click += (sender, args) => { };
-            RemoveAccount.Click += (sender, args) => { };
+            RemoveAccount.Click += async (sender, args) => 
+            {
+                if (AccountsList.SelectedIndex == -1)
+                {
+                    await new MessageDialog("Please select Account you want to remove.").ShowAsync();
+                    return;
+                }
+
+                Account account = AccountsList.SelectedItem as Account;
+                if (account == null) return;
+
+                Debug.WriteLine($"Remove Account {account.AccountName}");
+
+                // Remove Account form AccountsList
+                ViewModel.AccountsList.Remove(account);
+
+                // Save AccoutsList
+                SettingsController.Instance.localSettings.Values["AccountsList"] =
+                XmlSerialization<ObservableCollection<Account>>.Serialize(ViewModel.AccountsList);
+            };
+
             EditAccount.Click += (sender, args) => { };
         }
     }
