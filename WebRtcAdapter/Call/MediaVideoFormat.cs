96fb7e96a4b90bf21b3674eb0191c91b5925bd0c
ivanab <ivana@opticaltone.com>
2019-05-29 21:59:25 +0200

    Added MediaVideoFormat class

---
+++ WebRtcAdapter/Call/MediaVideoFormat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using ClientCore.Call;
+
+namespace WebRtcAdapter.Call
+{
+    class MediaVideoFormat : IMediaVideoFormat
+    {
+        public string Id { get; private set; }
+
+        public Size Dimension { get; private set; }
+
+        public IList<int> FrameRates { get; private set; }
+
+        public string GetId(string id)
+        {
+            return id;
+        }
+
+        public Size GetDimension(int width, int height)
+        {
+            Size size = new Size();
+            size.Width = width;
+            size.Height = height;
+
+            return size;
+        }
+
+        public IList<int> GetFrameRates(List<int> frameRates)
+        {
+            return frameRates;
+        }
+    }
+}
